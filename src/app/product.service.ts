a25e4447a9c388e9f89a8f890add9190112527e0
GitLito <jericho.pascua@gmail.com>
2020-04-23 23:24:35 +0800

    check in 04232020

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -24,6 +24,7 @@ export class ProductService {
 
     // getAlbum(id:number){
     getAlbum(id:number): Observable<Album>{    
+      // return this._http.get(this._albumUrl).map(response => <Album>response.json());
       return this._http.get(this._albumUrl).map(response => <Album>response.json());
     }   
 }
