1eb79e8b931bb2fffee1b850fade52a60b15ca99
GitLito <jericho.pascua@gmail.com>
2020-04-25 17:38:28 +0800

    tetst1

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 import { ProductService } from '../product.service';
 
-// import { Album } from '../album';
-
 
 @Component({
   selector: 'app-product-description',
