e098ed38fa9e0acf7ae51a67612a9c26a571efbf
GitLito <jericho.pascua@gmail.com>
2020-05-23 17:25:11 +0800

    est

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,8 +17,8 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule, Routes} from '@angular/router';
 
 const appRoutes: Routes = [ 
-  { path: 'products', component: ProductListComponent},
-  { path: 'produts/:id', component: ProductPageComponent},
+  { path: 'products', component: ProductListComponent },
+  { path: 'produts/:id', component: ProductPageComponent },
   { path: '', redirectTo: 'products',pathMatch: 'full'} 
 ];
 
