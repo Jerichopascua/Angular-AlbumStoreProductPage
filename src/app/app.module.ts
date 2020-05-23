b4b8e728243ca041e9d4983b85f328f83c51327d
GitLito <jericho.pascua@gmail.com>
2020-05-23 17:32:25 +0800

    est

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,11 +16,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 
 import { RouterModule, Routes} from '@angular/router';
 
-const appRoutes: Routes = [ 
-  { path: 'products', component: ProductListComponent },
-  { path: 'products/:id', component: ProductPageComponent },  
-  { path: '', redirectTo: 'products',pathMatch: 'full'} 
-];
+
 
 @NgModule({
   declarations: [
