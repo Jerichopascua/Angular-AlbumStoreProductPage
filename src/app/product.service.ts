40d047c9b84e5adc812cb335b078106175adbb57
GitLito <jericho.pascua@gmail.com>
2020-05-23 16:04:00 +0800

    test

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -29,7 +29,7 @@ export class ProductService {
 
  private _albumUrl ='../assets/album.json';
 
- private _productsUrl='..assets/products.json';
+ private _productsUrl ='../assets/products.json';
 
   constructor(private _http: Http) { }
 
