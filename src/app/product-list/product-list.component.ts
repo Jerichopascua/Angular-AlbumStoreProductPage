8502256a12b12bf2ad550ef19a6cf00fac56c59f
GitLito <jericho.pascua@gmail.com>
2020-05-23 16:17:54 +0800

    test

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -17,7 +17,7 @@ export class ProductListComponent implements OnInit {
 
   constructor(private _productService: ProductService) { }
   
-  ngOnInit(): void {
+  ngOnInit(){
       this._productService.getProducts().subscribe(response => this.products=response);      
   }
 
